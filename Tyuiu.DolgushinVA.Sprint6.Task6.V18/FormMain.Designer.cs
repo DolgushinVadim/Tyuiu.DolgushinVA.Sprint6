b613aa58c43a976fdd2e165b29c94c85da4cc704
Tyuiu.DolgushinVA@outlook.com <vadimdolgushin9@gmail.com>
2023-12-10 16:33:47 +0500

    Task6

--- Tyuiu.DolgushinVA.Sprint6.Task6.V18/FormMain.Designer.cs
+++ Tyuiu.DolgushinVA.Sprint6.Task6.V18/FormMain.Designer.cs
@@ -31,9 +31,6 @@ namespace Tyuiu.DolgushinVA.Sprint6.Task6.V18
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            this.panelTop_DVA = new System.Windows.Forms.Panel();
-            this.groupBoxTask_DVA = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panelLeft_DVA = new System.Windows.Forms.Panel();
             this.groupBoxDataInput_DVA = new System.Windows.Forms.GroupBox();
             this.textBoxLoadFromFile_DVA = new System.Windows.Forms.TextBox();
@@ -43,28 +40,92 @@ namespace Tyuiu.DolgushinVA.Sprint6.Task6.V18
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.openFileDialogTask_DVA = new System.Windows.Forms.OpenFileDialog();
             this.toolTip_DVA = new System.Windows.Forms.ToolTip(this.components);
-            this.buttonOpenFile_DVA = new System.Windows.Forms.Button();
-            this.buttonHelp_DVA = new System.Windows.Forms.Button();
+            this.groupBoxTask_DVA = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.buttonDone_DVA = new System.Windows.Forms.Button();
-            this.panelTop_DVA.SuspendLayout();
-            this.groupBoxTask_DVA.SuspendLayout();
+            this.buttonHelp_DVA = new System.Windows.Forms.Button();
+            this.buttonOpenFile_DVA = new System.Windows.Forms.Button();
+            this.panelTop_DVA = new System.Windows.Forms.Panel();
             this.panelLeft_DVA.SuspendLayout();
             this.groupBoxDataInput_DVA.SuspendLayout();
             this.panelFill_DVA.SuspendLayout();
             this.groupBoxDataOutput_DVA.SuspendLayout();
+            this.groupBoxTask_DVA.SuspendLayout();
+            this.panelTop_DVA.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panelTop_DVA
+            // panelLeft_DVA
             // 
-            this.panelTop_DVA.Controls.Add(this.buttonOpenFile_DVA);
-            this.panelTop_DVA.Controls.Add(this.buttonHelp_DVA);
-            this.panelTop_DVA.Controls.Add(this.buttonDone_DVA);
-            this.panelTop_DVA.Controls.Add(this.groupBoxTask_DVA);
-            this.panelTop_DVA.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop_DVA.Location = new System.Drawing.Point(0, 0);
-            this.panelTop_DVA.Name = "panelTop_DVA";
-            this.panelTop_DVA.Size = new System.Drawing.Size(1143, 170);
-            this.panelTop_DVA.TabIndex = 0;
+            this.panelLeft_DVA.Controls.Add(this.groupBoxDataInput_DVA);
+            this.panelLeft_DVA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft_DVA.Location = new System.Drawing.Point(0, 170);
+            this.panelLeft_DVA.Name = "panelLeft_DVA";
+            this.panelLeft_DVA.Size = new System.Drawing.Size(558, 428);
+            this.panelLeft_DVA.TabIndex = 1;
+            // 
+            // groupBoxDataInput_DVA
+            // 
+            this.groupBoxDataInput_DVA.Controls.Add(this.textBoxLoadFromFile_DVA);
+            this.groupBoxDataInput_DVA.Location = new System.Drawing.Point(12, 6);
+            this.groupBoxDataInput_DVA.Name = "groupBoxDataInput_DVA";
+            this.groupBoxDataInput_DVA.Size = new System.Drawing.Size(540, 410);
+            this.groupBoxDataInput_DVA.TabIndex = 0;
+            this.groupBoxDataInput_DVA.TabStop = false;
+            this.groupBoxDataInput_DVA.Text = "Ввод данных:";
+            // 
+            // textBoxLoadFromFile_DVA
+            // 
+            this.textBoxLoadFromFile_DVA.Location = new System.Drawing.Point(6, 21);
+            this.textBoxLoadFromFile_DVA.Multiline = true;
+            this.textBoxLoadFromFile_DVA.Name = "textBoxLoadFromFile_DVA";
+            this.textBoxLoadFromFile_DVA.ReadOnly = true;
+            this.textBoxLoadFromFile_DVA.Size = new System.Drawing.Size(528, 383);
+            this.textBoxLoadFromFile_DVA.TabIndex = 0;
+            // 
+            // panelFill_DVA
+            // 
+            this.panelFill_DVA.Controls.Add(this.groupBoxDataOutput_DVA);
+            this.panelFill_DVA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFill_DVA.Location = new System.Drawing.Point(558, 170);
+            this.panelFill_DVA.Name = "panelFill_DVA";
+            this.panelFill_DVA.Size = new System.Drawing.Size(585, 428);
+            this.panelFill_DVA.TabIndex = 2;
+            // 
+            // groupBoxDataOutput_DVA
+            // 
+            this.groupBoxDataOutput_DVA.Controls.Add(this.textBoxResult_DVA);
+            this.groupBoxDataOutput_DVA.Location = new System.Drawing.Point(9, 6);
+            this.groupBoxDataOutput_DVA.Name = "groupBoxDataOutput_DVA";
+            this.groupBoxDataOutput_DVA.Size = new System.Drawing.Size(564, 410);
+            this.groupBoxDataOutput_DVA.TabIndex = 0;
+            this.groupBoxDataOutput_DVA.TabStop = false;
+            this.groupBoxDataOutput_DVA.Text = "Вывод данных:";
+            // 
+            // textBoxResult_DVA
+            // 
+            this.textBoxResult_DVA.Location = new System.Drawing.Point(6, 21);
+            this.textBoxResult_DVA.Multiline = true;
+            this.textBoxResult_DVA.Name = "textBoxResult_DVA";
+            this.textBoxResult_DVA.ReadOnly = true;
+            this.textBoxResult_DVA.Size = new System.Drawing.Size(552, 383);
+            this.textBoxResult_DVA.TabIndex = 0;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(558, 170);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 428);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
+            // 
+            // openFileDialogTask_DVA
+            // 
+            this.openFileDialogTask_DVA.FileName = "openFileDialog1";
+            // 
+            // toolTip_DVA
+            // 
+            this.toolTip_DVA.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip_DVA.ToolTipTitle = "Подсказка";
             // 
             // groupBoxTask_DVA
             // 
@@ -86,101 +147,6 @@ namespace Tyuiu.DolgushinVA.Sprint6.Task6.V18
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
-            // panelLeft_DVA
-            // 
-            this.panelLeft_DVA.Controls.Add(this.groupBoxDataInput_DVA);
-            this.panelLeft_DVA.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeft_DVA.Location = new System.Drawing.Point(0, 170);
-            this.panelLeft_DVA.Name = "panelLeft_DVA";
-            this.panelLeft_DVA.Size = new System.Drawing.Size(558, 428);
-            this.panelLeft_DVA.TabIndex = 1;
-            // 
-            // groupBoxDataInput_DVA
-            // 
-            this.groupBoxDataInput_DVA.Controls.Add(this.textBoxLoadFromFile_DVA);
-            this.groupBoxDataInput_DVA.Location = new System.Drawing.Point(12, 6);
-            this.groupBoxDataInput_DVA.Name = "groupBoxDataInput_DVA";
-            this.groupBoxDataInput_DVA.Size = new System.Drawing.Size(540, 386);
-            this.groupBoxDataInput_DVA.TabIndex = 0;
-            this.groupBoxDataInput_DVA.TabStop = false;
-            this.groupBoxDataInput_DVA.Text = "Ввод данных:";
-            // 
-            // textBoxLoadFromFile_DVA
-            // 
-            this.textBoxLoadFromFile_DVA.Location = new System.Drawing.Point(6, 21);
-            this.textBoxLoadFromFile_DVA.Multiline = true;
-            this.textBoxLoadFromFile_DVA.Name = "textBoxLoadFromFile_DVA";
-            this.textBoxLoadFromFile_DVA.ReadOnly = true;
-            this.textBoxLoadFromFile_DVA.Size = new System.Drawing.Size(528, 389);
-            this.textBoxLoadFromFile_DVA.TabIndex = 0;
-            // 
-            // panelFill_DVA
-            // 
-            this.panelFill_DVA.Controls.Add(this.groupBoxDataOutput_DVA);
-            this.panelFill_DVA.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelFill_DVA.Location = new System.Drawing.Point(558, 170);
-            this.panelFill_DVA.Name = "panelFill_DVA";
-            this.panelFill_DVA.Size = new System.Drawing.Size(585, 428);
-            this.panelFill_DVA.TabIndex = 2;
-            // 
-            // groupBoxDataOutput_DVA
-            // 
-            this.groupBoxDataOutput_DVA.Controls.Add(this.textBoxResult_DVA);
-            this.groupBoxDataOutput_DVA.Location = new System.Drawing.Point(9, 6);
-            this.groupBoxDataOutput_DVA.Name = "groupBoxDataOutput_DVA";
-            this.groupBoxDataOutput_DVA.Size = new System.Drawing.Size(564, 389);
-            this.groupBoxDataOutput_DVA.TabIndex = 0;
-            this.groupBoxDataOutput_DVA.TabStop = false;
-            this.groupBoxDataOutput_DVA.Text = "Вывод данных:";
-            // 
-            // textBoxResult_DVA
-            // 
-            this.textBoxResult_DVA.Location = new System.Drawing.Point(6, 21);
-            this.textBoxResult_DVA.Multiline = true;
-            this.textBoxResult_DVA.Name = "textBoxResult_DVA";
-            this.textBoxResult_DVA.ReadOnly = true;
-            this.textBoxResult_DVA.Size = new System.Drawing.Size(552, 389);
-            this.textBoxResult_DVA.TabIndex = 0;
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(558, 170);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 428);
-            this.splitter1.TabIndex = 3;
-            this.splitter1.TabStop = false;
-            // 
-            // openFileDialogTask_DVA
-            // 
-            this.openFileDialogTask_DVA.FileName = "openFileDialog1";
-            // 
-            // toolTip_DVA
-            // 
-            this.toolTip_DVA.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.toolTip_DVA.ToolTipTitle = "Подсказка";
-            // 
-            // buttonOpenFile_DVA
-            // 
-            this.buttonOpenFile_DVA.Image = global::Tyuiu.DolgushinVA.Sprint6.Task6.V18.Properties.Resources.folder_page;
-            this.buttonOpenFile_DVA.Location = new System.Drawing.Point(18, 12);
-            this.buttonOpenFile_DVA.Name = "buttonOpenFile_DVA";
-            this.buttonOpenFile_DVA.Size = new System.Drawing.Size(98, 69);
-            this.buttonOpenFile_DVA.TabIndex = 3;
-            this.toolTip_DVA.SetToolTip(this.buttonOpenFile_DVA, "Открыть файл\r\nВыберите нужный файл для обработки");
-            this.buttonOpenFile_DVA.UseVisualStyleBackColor = true;
-            this.buttonOpenFile_DVA.Click += new System.EventHandler(this.buttonOpenFile_Click);
-            // 
-            // buttonHelp_DVA
-            // 
-            this.buttonHelp_DVA.Image = global::Tyuiu.DolgushinVA.Sprint6.Task6.V18.Properties.Resources.help;
-            this.buttonHelp_DVA.Location = new System.Drawing.Point(1032, 12);
-            this.buttonHelp_DVA.Name = "buttonHelp_DVA";
-            this.buttonHelp_DVA.Size = new System.Drawing.Size(93, 69);
-            this.buttonHelp_DVA.TabIndex = 2;
-            this.toolTip_DVA.SetToolTip(this.buttonHelp_DVA, "Информация о программе");
-            this.buttonHelp_DVA.UseVisualStyleBackColor = true;
-            this.buttonHelp_DVA.Click += new System.EventHandler(this.buttonHelp_Click);
-            // 
             // buttonDone_DVA
             // 
             this.buttonDone_DVA.Enabled = false;
@@ -194,6 +160,40 @@ namespace Tyuiu.DolgushinVA.Sprint6.Task6.V18
             this.buttonDone_DVA.UseVisualStyleBackColor = true;
             this.buttonDone_DVA.Click += new System.EventHandler(this.buttonDone_DVA_Click);
             // 
+            // buttonHelp_DVA
+            // 
+            this.buttonHelp_DVA.Image = global::Tyuiu.DolgushinVA.Sprint6.Task6.V18.Properties.Resources.help;
+            this.buttonHelp_DVA.Location = new System.Drawing.Point(1032, 12);
+            this.buttonHelp_DVA.Name = "buttonHelp_DVA";
+            this.buttonHelp_DVA.Size = new System.Drawing.Size(93, 69);
+            this.buttonHelp_DVA.TabIndex = 2;
+            this.toolTip_DVA.SetToolTip(this.buttonHelp_DVA, "Информация о программе");
+            this.buttonHelp_DVA.UseVisualStyleBackColor = true;
+            this.buttonHelp_DVA.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // buttonOpenFile_DVA
+            // 
+            this.buttonOpenFile_DVA.Image = global::Tyuiu.DolgushinVA.Sprint6.Task6.V18.Properties.Resources.folder_page;
+            this.buttonOpenFile_DVA.Location = new System.Drawing.Point(18, 12);
+            this.buttonOpenFile_DVA.Name = "buttonOpenFile_DVA";
+            this.buttonOpenFile_DVA.Size = new System.Drawing.Size(98, 69);
+            this.buttonOpenFile_DVA.TabIndex = 3;
+            this.toolTip_DVA.SetToolTip(this.buttonOpenFile_DVA, "Открыть файл\r\nВыберите нужный файл для обработки");
+            this.buttonOpenFile_DVA.UseVisualStyleBackColor = true;
+            this.buttonOpenFile_DVA.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // panelTop_DVA
+            // 
+            this.panelTop_DVA.Controls.Add(this.buttonOpenFile_DVA);
+            this.panelTop_DVA.Controls.Add(this.buttonHelp_DVA);
+            this.panelTop_DVA.Controls.Add(this.buttonDone_DVA);
+            this.panelTop_DVA.Controls.Add(this.groupBoxTask_DVA);
+            this.panelTop_DVA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop_DVA.Location = new System.Drawing.Point(0, 0);
+            this.panelTop_DVA.Name = "panelTop_DVA";
+            this.panelTop_DVA.Size = new System.Drawing.Size(1143, 170);
+            this.panelTop_DVA.TabIndex = 0;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -203,38 +203,38 @@ namespace Tyuiu.DolgushinVA.Sprint6.Task6.V18
             this.Controls.Add(this.panelFill_DVA);
             this.Controls.Add(this.panelLeft_DVA);
             this.Controls.Add(this.panelTop_DVA);
+            this.MaximizeBox = false;
             this.Name = "FormMain";
             this.Text = "Спринт 6 | Таск 6 | Вариант 18 | Долгушин В. А.";
-            this.panelTop_DVA.ResumeLayout(false);
-            this.groupBoxTask_DVA.ResumeLayout(false);
-            this.groupBoxTask_DVA.PerformLayout();
             this.panelLeft_DVA.ResumeLayout(false);
             this.groupBoxDataInput_DVA.ResumeLayout(false);
             this.groupBoxDataInput_DVA.PerformLayout();
             this.panelFill_DVA.ResumeLayout(false);
             this.groupBoxDataOutput_DVA.ResumeLayout(false);
             this.groupBoxDataOutput_DVA.PerformLayout();
+            this.groupBoxTask_DVA.ResumeLayout(false);
+            this.groupBoxTask_DVA.PerformLayout();
+            this.panelTop_DVA.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panelTop_DVA;
-        private System.Windows.Forms.GroupBox groupBoxTask_DVA;
         private System.Windows.Forms.Panel panelLeft_DVA;
         private System.Windows.Forms.Panel panelFill_DVA;
         private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.GroupBox groupBoxDataInput_DVA;
         private System.Windows.Forms.GroupBox groupBoxDataOutput_DVA;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBoxLoadFromFile_DVA;
         private System.Windows.Forms.TextBox textBoxResult_DVA;
         private System.Windows.Forms.OpenFileDialog openFileDialogTask_DVA;
         private System.Windows.Forms.ToolTip toolTip_DVA;
-        private System.Windows.Forms.Button buttonOpenFile_DVA;
-        private System.Windows.Forms.Button buttonHelp_DVA;
+        private System.Windows.Forms.GroupBox groupBoxTask_DVA;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button buttonDone_DVA;
+        private System.Windows.Forms.Button buttonHelp_DVA;
+        private System.Windows.Forms.Button buttonOpenFile_DVA;
+        private System.Windows.Forms.Panel panelTop_DVA;
     }
 }
 
